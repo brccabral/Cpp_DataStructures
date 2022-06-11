38aa6564e3ddbfc8460e020a0d227595e73efb1b
Bruno Cabral <cabral.brc@gmail.com>
2022-06-11 12:56:43 -0700

    implemented insert end and delete at position

--- linked_list.cpp
+++ linked_list.cpp
@@ -9,6 +9,8 @@ typedef struct Node
 
 void Insert(int data, int pos);
 void Print();
+void InsertEnd(int data);
+void Delete(int pos);
 
 Node *head;
 
@@ -16,10 +18,20 @@ int main()
 {
     head = NULL;
 
-    Insert(2, 1); // 2
-    Insert(3, 2); // 2,3
-    Insert(4, 1); // 4,2,3
-    Insert(5, 2); // 4,5,2,3
+    // Insert(2, 1); // 2
+    // Insert(3, 2); // 2,3
+    // Insert(4, 1); // 4,2,3
+    // Insert(5, 2); // 4,5,2,3
+    InsertEnd(2);
+    InsertEnd(4);
+    InsertEnd(6);
+    InsertEnd(5); // 2,4,6,5
+    Print();
+
+    printf("Enter a position to delete\n");
+    int d;
+    scanf("%d", &d);
+    Delete(d);
     Print();
 
     return (0);
@@ -59,4 +71,46 @@ void Print()
         temp = temp->next;
     }
     printf("\n");
+}
+
+void InsertEnd(int data)
+{
+    Node *temp = new Node();
+    temp->data = data;
+    temp->next = NULL;
+
+    if (head == NULL)
+    {
+        head = temp;
+        return;
+    }
+    Node *temp2 = head;
+    while (temp2->next != NULL)
+    {
+        temp2 = temp2->next;
+    }
+    temp->next = temp2->next;
+    temp2->next = temp;
+}
+
+void Delete(int pos)
+{
+    Node *temp = head;
+    if (pos == 1)
+    {
+        head = temp->next;
+        free(temp);
+        return;
+    }
+    for (int i = 1; i < pos - 1; i++)
+    {
+        if (temp->next == NULL)
+            break;
+        temp = temp->next;
+    }
+    Node *temp2 = temp->next;
+    if (temp2 != NULL)
+        temp->next = temp2->next;
+
+    free(temp2);
 }
